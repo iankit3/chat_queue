f4c4df4190b83d319c8021a4edf99b899cc428d9
iankit3 <a4ankit@outlook.com>
2020-04-20 22:27:47 +0530

    boot-web, api

---
+++ rabbitmq_java/messaging/src/main/java/com/ankit/models/Message.java
@@ -0,0 +1,27 @@
+package com.ankit.models;
+
+public class Message {
+    private String data;
+    private String info;
+
+    public Message(String data){
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+}
